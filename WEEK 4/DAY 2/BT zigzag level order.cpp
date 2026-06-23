49a885f5189cc7847a5a2e0d6eb3efa20ec536fd
ASTHAGUPTA-co <asthagupta506@gmail.com>
2026-06-23 22:21:40 +0530

    Create BT zigzag level order.cpp

---
+++ WEEK 4/DAY 2/BT zigzag level order.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        int levelno=0;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+           int n = q.size();
+           vector<int>level;
+           for(int i=0;i<n;i++){
+             TreeNode* node = q.front();
+             q.pop();
+             level.push_back(node->val);
+             if(node->left) q.push(node->left);
+             if(node->right) q.push(node->right);
+           }
+           if(levelno%2==0){
+             ans.push_back(level);
+           }else{
+            reverse(level.begin(),level.end());
+            ans.push_back(level);
+           }
+           levelno++;
+        }
+        return ans;
+    }
+};
